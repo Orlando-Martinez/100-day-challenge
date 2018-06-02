f5d0b2eddcfd853f141d08c139a81f0b9d459dbd
orlando-martinez <omartinezweb@gmail.com>
2018-06-02 17:00:21 -0400

    Stateless Button component.

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button'
 
 class App extends Component {
+
+  alert = () => {
+    alert('Click handler worked!')
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">100-Day Coding Challenge</h1>
+          <Button text={'Click Me'} handleClick={this.alert}/>
         </header>
         <p className="App-intro">
           <ul>
