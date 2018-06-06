7bb7de7451ab9db227beed9a410a4c34d586a586
orlando-martinez <omartinezweb@gmail.com>
2018-06-06 19:20:20 -0400

    comparison between startDate and endDate in a functional manner.

--- app/src/components/Countdown/index.js
+++ app/src/components/Countdown/index.js
@@ -4,11 +4,13 @@ class Countdown extends Component {
     daysLeft = () => {
         var startDate = new Date(this.props.start);
         var endDate = new Date(this.props.end);
-
         var timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
-        var daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-        return daysLeft;
+        var daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        const result = this.isTimeUp(daysLeft) ? 'Congratulations! You completeted the challenge!' : `${daysLeft} days left`
+        return result;
     }
+
+    isTimeUp = (daysLeft) => daysLeft === 0
 
     render() {
         return (
