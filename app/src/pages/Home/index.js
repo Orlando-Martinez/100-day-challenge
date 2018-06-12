5198de7f22d05d1a5b8ec7d449a0c7446c2e29ff
orlando-martinez <omartinezweb@gmail.com>
2018-06-11 22:52:16 -0400

    display svg image through webpack.

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './styles.scss'
 
+import searchIcon from '../../images/search.svg'
+
 const Home = () => (
-    <div className='Home'>Home</div>
+    <div className='Home'>Home
+        <img src={searchIcon} className='search' />
+    </div>
 );
 
 export default Home;
