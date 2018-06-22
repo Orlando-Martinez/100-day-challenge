8c47de71f497e15ee51d223628374adb98149943
orlando-martinez <omartinezweb@gmail.com>
2018-06-22 19:45:37 -0400

    style updates + About is not stateful component.

--- app/src/pages/About/index.js
+++ app/src/pages/About/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.scss';
 import GitHub from '../../images/GitHub.png';
 import linkedIn from '../../images/linkedIn.png';
@@ -7,31 +7,40 @@ import Social from '../../components/Social';
 import RIT from '../../images/RIT.jpg';
 import loadGoogleMapsApi from 'load-google-maps-api';
 
-loadGoogleMapsApi().then(function (googleMaps) {
-    new googleMaps.Map(document.querySelector('.map'), {
-      center: {
-        lat: 40.7484405,
-        lng: -73.9944191
-      },
-      zoom: 12
-    })
-  }).catch(function (error) {
-    console.error(error)
-})
+class About extends Component {
+    constructor(props) {
+        super(props);
+        loadGoogleMapsApi().then(function (googleMaps) {
+            const map = new googleMaps.Map(document.querySelector('.map'), {
+              center: {
+                lat: 40.7484405,
+                lng: -73.9944191
+              },
+              zoom: 12
+            })
+          }).catch(function (error) {
+            console.error(error)
+        })
+    }
 
-const About = () => (
-    <div className='about'>
-        <Social image={GitHub} url={'https://github.com/Orlando-Martinez'} />
-        <Social image={linkedIn} url={'https://www.linkedin.com/in/orlando-martinez-42755850/'} />
-        <div>
-            <img src={RIT} className='rit' alt='rit' />
-            <h2>Rochester Institue of Technology</h2>
-            <p>Class of 2014</p>
-            <p>Bachelors of Science New Media Interactive Development</p>
-            <p>Minor Music Technology</p>
+    test = () => {
+        console.log('clicked map')
+    }
+    
+    render = () => (
+        <div className='about'>
+            <Social image={GitHub} url={'https://github.com/Orlando-Martinez'} />
+            <Social image={linkedIn} url={'https://www.linkedin.com/in/orlando-martinez-42755850/'} />
+            <div className='college'>
+                <img src={RIT} className='rit' alt='rit' />
+                <h2>Rochester Institue of Technology</h2>
+                <p>Class of 2014</p>
+                <p>Bachelors of Science in New Media Interactive Development</p>
+                <p>Minor Music Technology</p>
+            </div>
+            <div className='map' onClick={this.test}></div>
         </div>
-        <div className='map'></div>
-    </div>
-);
+    )
+};
 
 export default About;
